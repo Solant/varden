c565d7dd5d90d1f1663ff0157425e115aaf036dd
Solant <runner62v6@gmail.com>
2026-05-10 22:46:53 +0200

    test: split node and browser test runners

--- packages/varden/vitest.config.ts
+++ packages/varden/vitest.config.ts
@@ -2,14 +2,31 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
   test: {
-    browser: {
-      enabled: true,
-      provider: 'playwright',
-      headless: true,
-      instances: [{ browser: 'chromium' }],
-      screenshot: 'off',
-    },
+    projects: [
+      {
+        test: {
+          // an example of file based convention,
+          // you don't have to follow it
+          include: ['src/*.spec.ts'],
+          name: 'unit',
+          environment: 'node',
+        },
+      },
+      {
+        plugins: [vue()],
+        test: {
+          include: ['src/components/*.spec.ts'],
+          name: 'browser',
+          environment: 'node',
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+        },
+      },
+    ],
   },
 });
