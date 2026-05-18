8bcf3ed70651ed6cd47caa1589857f398c5b6838
Solant <runner62v6@gmail.com>
2026-05-19 00:23:53 +0200

    chore: upgrade eslint v10 (#10)
    * chore: update eslint
    
    * chore: remove duplicated workspace dependency

--- packages/varden/src/path.ts
+++ packages/varden/src/path.ts
@@ -1,11 +1,10 @@
-export type Paths<T> =
-  T extends Array<infer U>
-    ? `${Paths<U>}`
-    : T extends object
-      ? {
-        [K in keyof T & (string | number)]: K extends string ? `${K}` | `${K}.${Paths<T[K]>}` : never;
-      }[keyof T & (string | number)]
-      : never;
+export type Paths<T> = T extends Array<infer U>
+  ? `${Paths<U>}`
+  : T extends object
+    ? {
+      [K in keyof T & (string | number)]: K extends string ? `${K}` | `${K}.${Paths<T[K]>}` : never;
+    }[keyof T & (string | number)]
+    : never;
 
 export type Get<T, P extends Paths<T>> = P extends `${infer K}.${infer R}`
   ? K extends keyof T
